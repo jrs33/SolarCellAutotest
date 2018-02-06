f84b3befea486df17492e0861ddb968cad2794bc
jrs33 <surettej@bu.edu>
2018-02-05 21:07:32 -0500

    Adding in the finalized driver file without error handling fully implemented

--- EDSControlFactory.py
+++ EDSControlFactory.py
@@ -18,6 +18,7 @@ class EDSControlFactory(object):
 
     def engagePowerSupplyAndClean(self):
         PIN = 22
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(PIN, GPIO.OUT)
         GPIO.output(PIN, True)
 
--- driver.py
+++ driver.py
@@ -4,37 +4,48 @@ import RPi.GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
+from datetime import datetime
+
 from GPIOControlFactory import *
 from ADCControlFactory import *
 from DataTransportFactory import *
+from EDSControlFactory import *
 
 '''
 This is an abstracted function to run the logic
 for our EDS tests. The tests go as follows:
 
 1) Run test on selected cell without the EDS running
-2) Record the data from this these locally and remotely
+2) Record the data from this test
 3) Pause the test and turn on the EDS for 2 minutes
 4) Turn off the EDS
 5) Repeat 1 & 2
-6) Compare the ratios of before and after cleaning to get
-EDS effectiveness
+6) Compare the ratios of before and after cleaning and
+store data locally and remotely 
 '''
 def runEDSTest():
     gpio = GPIOControlFactory(0)
     adc = ADCControlFactory(0)
     transporter = DataTransportFactory(0)
+    eds = EDSControlFactory(0)
 
-    # Step 1
+    # Step 1 and 2
     selectedCell = gpio.cellSelect()
     gpio.engageGPIO(selectedCell)
-    average = adc.gatherADCData()
+    averagePreClean = adc.gatherADCData()
     gpio.disengageGPIO(selectedCell)
 
-    # Step 2
-    transporter.transportToUSB(average)
-
     # Step 3
-    
+    eds.engagePowerSupplyAndClean()
+    eds.disengagePowerSupply()
+
+    # Step 4
+    gpio.engageGPIO(selectedCell)
+    averagePostClean = adc.gatherADCData()
+    gpio.disengageGPIO(selectedCell)
+
+    # Step 5
+    ratio = averagePostClean/averagePreClean
+    transporter.transportToUSB(ratio,selectedCell,str(datetime.now()))
 
 runEDSTest()
