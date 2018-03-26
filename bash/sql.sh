5172385e5137d9833d04d05cd3fca1b9ea4bffc2
Josh <surettej@bu.edu>
2018-03-26 10:46:25 -0400

    adding database initialization file

---
+++ bash/sql.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SQLITE_FILE="../solarbytes.db"
+TABLE_NAME="solarTests"
+
+function check_db_exists() {
+	local return_flag=1
+	if [ -e $SQLITE_FILE ]; then
+		local return_flag=0
+	fi
+	return return_flag
+}
+
+function create_database() {
+	if [ check_db_exists == 0 ]; then
+		touch $SQLITE_FILE
+		echo "database created"
+	fi
+	echo "error: database already exists"
+	exit
+}
+
+function create_table() {
+	sqlite3 $SQLITE_FILE "BEGIN; CREATE TABLE $TABLE_NAME (testDate DATE, testTime TIME, cell INTEGER, ratio FLOAT, temp FLOAT, humidity FLOAT); COMMIT;"
+}
+
+create_database
+create_table
