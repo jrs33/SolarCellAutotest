50bcbe679015014be5d42b4a06bd83c228c51dd8
jrs33 <surettej@bu.edu>
2018-04-29 19:42:30 +0000

    modifying test script to default to bulk cell test

--- bash/runTest.sh
+++ bash/runTest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 FILE="../solarCellTestDriver.py"
-FOURTH_CELL=21
+ALL_CELLS=0
 USERNAME=""
 PASSWORD=""
 
 # checks for existence of file and runs the test
 function run_test_file() {
-	curl -s -u $USERNAME:$PASSWORD http://localhost:5000/testEDS/$FOURTH_CELL > /dev/null
+	curl -s -u $USERNAME:$PASSWORD http://localhost:5000/testEDS/$ALL_CELLS > /dev/null
 }
 
 # run command
